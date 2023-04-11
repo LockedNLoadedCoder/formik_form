6b48a847ecb489418a6b0b88b4705051d5c03f18
Rocky Aguilar <rocky351ford@yahoo.com>
2023-04-10 19:14:18 -0700

    added formik form with validate

--- src/App.js
+++ src/App.js
@@ -8,20 +8,51 @@ function App() {
       email: '',
       password: '',
     },
-    onSubmit: values =>{
-      console.log('form:', values);
+    onSubmit: (values) =>{
+      alert("Login Successful");
+    },
+    validate: (values) => {
+      let errors = {};
+      if (!values.name) errors.name = "field required";
+      if (!values.email) errors.email = "field required";
+      if (!values.password) errors.password = "field required";
+      return errors;
     }
   });
+
   return (
     <div>
       <form onSubmit={formik.handleSubmit}>
         <div>Name</div>
         <input id="name" type="text" onChange={formik.handleChange} values={formik.values.name}/>
         <div>Email</div>
-        <input id="email" type="text" onChange={formik.handleChange} values={formik.values.email}/>
+        <input 
+          id="email" 
+          type="text" 
+          onChange={formik.handleChange}
+          values={formik.values.email}
+        />
+        {formik.errors.email ? (
+          <div id="emailError" style={{ color: "red" }}>
+            {formik.errors.email}
+          </div>
+        ) : null}
         <div>Password</div>
-        <input id="password" type="text" onChange={formik.handleChange} values={formik.values.password}/>
-        <button type="submit">Submit</button>
+        <input
+         id="password"
+         type="text"
+         onChange={formik.handleChange}
+         values={formik.values.password}
+        />
+        <br />
+        {formik.errors.password ? (
+          <div id="pswError" style={{ color: "red" }}>
+            {formik.errors.password}
+          </div>
+        ) : null}
+        <button id="submitBtn" type="submit">
+          Submit
+        </button>
       </form>
     </div>  
   );
